f5de827cd3a5a70839ea1de54cc2fe4bbe490452
paisal johen <paisal.eltibiz@gmail.com>
2026-09-22 17:23:26 +0700

    projeck udah selesai

--- app/Http/Controllers/PortfolioController.php
+++ app/Http/Controllers/PortfolioController.php
@@ -25,7 +25,6 @@ class PortfolioController extends Controller
 
         // PROJECTS
         $projects = Project::with('skills')
-            ->where('is_featured', true)
             ->latest()
             ->get();
 
